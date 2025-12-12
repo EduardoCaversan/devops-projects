74456e926e5b289560179bbbb7c4800c79868782
Eduardo Caversan <eduardo@eventodaigreja.com.br>
2025-12-11 22:07:22 -0300

    first commit

---
+++ kubernetes-deploy-sample/app/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Aplicação Node.js rodando! 🚀");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
